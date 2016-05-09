71fa9175022be6a0d6e3ad1277a8b82878af3206
Sharangdhar Chakraborty <sharangc@ghc41.ghc.andrew.cmu.edu>
2016-05-09 06:04:06 -0400

    final images left

--- webapp/urls.py
+++ webapp/urls.py
@@ -20,5 +20,5 @@ from django.conf.urls import include
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^chemposer/', include('chemposer.urls')),
-#    url(r'^$', include('chemposer.urls')),
+    url(r'^$', include('chemposer.urls')),
 ]
